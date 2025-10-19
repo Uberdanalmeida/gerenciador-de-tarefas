6431460befd7cbf17861668cbac655ea2bb10f70
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-10-18 21:55:58 -0300

    Estelizando meu h1, e as DIVs.

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,12 @@ import Task from './components/Task'
 
 function App() {
   return (
-    <div>
-      <h1>Gerenciador de Tarefas</h1>
-      <AddTask></AddTask>
-      <Task></Task>
+    <div className='h-screen w-screen bg-slate-500 flex justify-center p-6'>
+      <div className='w-[500px]'>
+        <h1 className='text-3xl text-slate-100 font-bold text-cenetr'>Gerenciador de Tarefas</h1>
+        <AddTask></AddTask>
+        <Task></Task>
+      </div>
     </div>
   )
 }
