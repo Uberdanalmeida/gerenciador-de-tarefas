e32deef0bff39a026776febd2f60c1bbe62fe5a6
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-10-19 23:37:52 -0300

    Criando e alterando meu State.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,49 @@
+import { useState } from 'react'
 import './App.css'
 import AddTask from './components/AddTask'
 import Task from './components/Task'
 
 function App() {
+
+const [list, setList] = useState ([
+  {
+    id: 1,
+    title: 'Estudar Programação',
+    description: 'Estudar, para tornar desenvolvedor',
+    isComplet: true
+  },
+
+  {
+    id: 2,
+    title: 'Estudar Inglês',
+    description: 'Estudar, para tornar desenvolvedor',
+    isComplet: false
+  },
+
+  {
+    id: 3,
+    title: 'Estudar Matematica',
+    description: 'Estudar, para tornar desenvolvedor',
+    isComplet: false
+  }
+])
+
+function onTaskClick(taskId) {
+  const newTask = list.map(list => {
+    if(list.id === taskId) {
+      return {...list, isComplet: !list.isComplet}
+    }
+    return list;
+  })
+  setList(newTask)
+}
+
   return (
     <div className='h-screen w-screen bg-slate-500 flex justify-center p-6'>
       <div className='w-[500px]'>
         <h1 className='text-3xl text-slate-100 font-bold text-cenetr'>Gerenciador de Tarefas</h1>
         <AddTask></AddTask>
-        <Task></Task>
+        <Task taskList={list} onTaskClick={onTaskClick}></Task>
       </div>
     </div>
   )
--- src/components/Task.jsx
+++ src/components/Task.jsx
@@ -1,6 +1,20 @@
-function Task() {
+import { list } from "postcss"
+
+function Task(props) {
     return(
-        <div></div>
+        <div>
+            <ul className="space-y-4 p-6 bg-slate-200 rounded-md shadow">
+                {props.taskList.map((list) => (
+                    
+                <li key={list.id} className="flex gap-2">
+                    <button onClick={() => props.onTaskClick(list.id)} className="bg-slate-400 text-left w-full text-white p-2 rounded-md ">{list.title}</button> 
+
+                    <button className="bg-slate-400 p-2 rounded-md text-white">Ver detalhes</button>
+                </li>
+                 ))
+                }
+            </ul>
+        </div>
     )
 }
 
