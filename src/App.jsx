73b2f15e451291dc2675c8f5af4219a16d3a613e
Uberdanalmeida <105062084+Uberdanalmeida@users.noreply.github.com>
2025-10-27 22:31:55 -0300

    Esconde a lista de Task, se a lista estiver vazia.

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,74 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import AddTask from './components/AddTask'
-import Task from './components/Task'
-import { v4 } from 'uuid'
-import Title from './components/Title'
+import { useEffect, useState } from "react";
+import "./App.css";
+import AddTask from "./components/AddTask";
+import Task from "./components/Task";
+import { v4 } from "uuid";
+import Title from "./components/Title";
 
 function App() {
+  const [list, setList] = useState(
+    JSON.parse(localStorage.getItem("list")) || []
+  );
 
-const [list, setList] = useState (
-JSON.parse(localStorage.getItem("list")) || []
-)
+  useEffect(() => {
+    localStorage.setItem("list", JSON.stringify(list));
+  }, [list]);
 
-useEffect(() => {
-  localStorage.setItem("list", JSON.stringify(list))
-}, [list])
-
-useEffect(() => {
-  async function fetchList() {
-    const response = await fetch('https://jsonplaceholder.typicode.com/todos?_limit=10', {
-    method: "GET"
-  })
-  const data = await response.json()
-  setList(data)
-  }
-  // fetchList()
-}, [])
-
-function onTaskClick(taskId) {
-  const newTask = list.map(list => {
-    if(list.id === taskId) {
-      return {...list, isComplet: !list.isComplet}
+  useEffect(() => {
+    async function fetchList() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/todos?_limit=10",
+        {
+          method: "GET",
+        }
+      );
+      const data = await response.json();
+      setList(data);
     }
-    return list;
-  })
-  setList(newTask)
-}
+    // fetchList()
+  }, []);
 
-function deleteTask(taskId) {
-  const newTask = list.filter((list) => list.id != taskId)
-  setList(newTask)
-}
+  function onTaskClick(taskId) {
+    const newTask = list.map((list) => {
+      if (list.id === taskId) {
+        return { ...list, isComplet: !list.isComplet };
+      }
+      return list;
+    });
+    setList(newTask);
+  }
 
-function AddTaskSubmit (title, description) {
+  function deleteTask(taskId) {
+    const newTask = list.filter((list) => list.id != taskId);
+    setList(newTask);
+  }
 
-  const newTask = {
+  function AddTaskSubmit(title, description) {
+    const newTask = {
       id: v4(),
       title,
       description,
-      isComplet: false
-  };
+      isComplet: false,
+    };
 
-  setList([...list, newTask])
-}
+    setList([...list, newTask]);
+  }
 
   return (
-    <div className='h-screen w-screen bg-slate-500 flex justify-center p-6'>
-      <div className='w-[500px] space-y-4'>
+    <div className="h-screen w-screen bg-slate-500 flex justify-center p-6">
+      <div className="w-[500px] space-y-4">
         <Title>Gerenciador de Tarefas</Title>
         <AddTask AddTaskSubmit={AddTaskSubmit}></AddTask>
-        <Task taskList={list} onTaskClick={onTaskClick} deleteTask={deleteTask}></Task>
+        {list.length ? (
+          <Task
+            taskList={list}
+            onTaskClick={onTaskClick}
+            deleteTask={deleteTask}
+          ></Task>
+        ) : null}
       </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
